26192be06bb36eb231091e359c860ad3fb0edb4f
Romain Fontaine <romain-fontaine@outlook.com>
2016-05-18 15:28:20 +0200

    sol3D

--- src/main.cpp
+++ src/main.cpp
@@ -9,56 +9,7 @@ inline int max(int x, int y){
 
 using namespace std;
 
-void solve2D(int k, int n, int m, int *X, int *Y, Solver &s){
-    int mu[k][n][m];
-    for(int h=0; h<k; h++){
-        vec<Lit> solution_exists;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                mu[h][i][j] = s.newVar();
-            }
-        }
-        for(int i=0; i<=n-X[h]; i++){
-            for(int j=0; j<=m-Y[h]; j++){
-                solution_exists.push(Lit(mu[h][i][j]));
-            }
-        }
-        for(int i=n-X[h]+1; i<n; i++){
-            for(int j=m-Y[h]+1; j<m; j++){
-                s.addUnit(~Lit(mu[h][i][j]));
-            }
-        }
-        s.addClause(solution_exists);
-    }
-
-    for(int h=0; h<k; h++){
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                for(int h2=0; h2<h; h2++){
-                    for(int i2=max(i-X[h2], 0);i2<n && i2<i+X[h]; i2++){
-                        for(int j2=max(j-Y[h2], 0);j2<m && j2<j+Y[h]; j2++){
-                            s.addBinary(~Lit(mu[h][i][j]), ~Lit(mu[h2][i2][j2]));
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    s.solve();
-    cout << s.okay() << endl;
-    for(int h =0; h<k; h++){
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(s.model[mu[h][i][j]] == l_True){
-                    cout << h + 1 <<" "<<i<<" "<<j<<endl;
-                }
-            }
-        }
-    }   
-}
-
-void newSolve2D(int k, int n, int m, int *X, int *Y){
+void Solve2D(int k, int n, int m, int *X, int *Y){
     Solver s;
     int mu[k][n][m][2];
     for(int i=0; i<k; i++){
@@ -108,54 +59,78 @@ void newSolve2D(int k, int n, int m, int *X, int *Y){
                     if(s.model[mu[i][x][y][0]] == l_True){
                         cout << ""<< i + 1 <<" "<<x<<" "<<y<<endl;
                     }
-                    if(s.model[mu[i][x][y][1]] == l_True){
-                       // cout << "r: "<< i + 1 <<" "<<x<<" "<<y<<endl;
-                    }
                 }
             }
         }   
     }
 }
 
-void solve3D(int k, int n, int m, int p, int *X, int *Y, int *Z, Solver &s){
-    int mu[k][n][m][p];
-    for(int h=0; h<k; h++){
+void solve3D(int k, int n, int m, int o, int *X, int *Y, int *Z){
+    Solver s;
+    int mu[k][n][m][o][2];
+    for(int i=0; i<k; i++){
+        for(int x=0; x<n; x++){
+            for(int y=0; y<m; y++){
+                for(int z=0; z<o; z++){
+                    mu[i][x][y][z][0] = s.newVar();
+                    mu[i][x][y][z][1] = s.newVar();
+                }
+            }
+        }
+    }
+
+    for(int i=0; i<k; i++){
         vec<Lit> solution_exists;
-        for(int i=0; i<n; i++)
-            for(int j=0; j<m; j++)
-                for(int g=0; g<p; g++)
-                    mu[h][i][j][g] = s.newVar();
-
-        for(int i=0; i<=n-X[h]; i++)
-            for(int j=0; j<=m-Y[h]; j++)
-                for(int g=0; g<=p-Z[h]; g++)
-                    solution_exists.push(Lit(mu[h][i][j][g]));
-
-        for(int i=n-X[h]+1; i<n; i++)
-            for(int j=m-Y[h]+1; j<m; j++)
-                for(int g=p-Z[h]+1; g<p; g++)
-                    s.addUnit(~Lit(mu[h][i][j][g]));
+        for(int x0=0; x0<=n-X[i]; x0++){
+            for(int y0=0; y0<=m-Y[i]; y0++){
+                for(int z0=0; z0<=o-Z[i]; z0++){
+                    solution_exists.push(Lit(mu[i][x0][y0][z0][0]));
+                    for(int x1=0; x1<n; x1++){
+                        for(int y1=0; y1<m; y1++){
+                            for(int z1=0; z1<o; z1++){
+                                if(x0<=x1 && x1<(x0+X[i])
+                                        && y0 <= y1 && y1<(y0+Y[i])
+                                        && z0 <= z1 && z1<(z0+Z[i])){
+                                    s.addBinary(~Lit(mu[i][x0][y0][z0][0]), Lit(mu[i][x1][y1][z1][1]));
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
         s.addClause(solution_exists);
     }
 
-    for(int h=0; h<k; h++)
-        for(int i=0; i<n; i++)
-            for(int j=0; j<m; j++)
-                for(int g=0; g<p; g++)
-                    for(int h2=0; h2<k && h2!=h; h2++)
-                        for(int i2=i;i2<n && i2<i+X[h]; i2++)
-                            for(int j2=j;j2<m && j2<j+Y[h]; j2++)
-                                for(int g2=g;g2<p && g2<g+Z[h]; g2++)
-                                    s.addBinary(~Lit(mu[h][i][j][g]), ~Lit(mu[h2][i2][j2][g2]));
+    for(int i=0; i<k; i++){
+        for(int j=0; j<k; j++){
+            if(i == j)
+                continue;
+            for(int x=0; x<n; x++){
+                for(int y=0; y<m; y++){
+                    for(int z=0; z<o; z++){
+                        s.addBinary(~Lit(mu[i][x][y][z][1]), ~Lit(mu[j][x][y][z][1]));
+                    }
+                }
+            }
+        }
+    }
 
     s.solve();
     cout << s.okay() << endl;
-    for(int h =0; h<k; h++)
-        for(int i=0; i<n; i++)
-            for(int j=0; j<m; j++)
-                for(int g=0; g<p; g++)
-                    if(s.model[mu[h][i][j][g]] == l_True)
-                        cout << h + 1 <<" "<<i<<" "<<j << " " << g <<endl;
+    if(s.okay()){
+        for(int i =0; i<k; i++){
+            for(int x=0; x<n; x++){
+                for(int y=0; y<m; y++){
+                    for(int z=0; z<o; z++){
+                        if(s.model[mu[i][x][y][z][0]] == l_True){
+                            cout << ""<< i + 1 <<" "<<x<<" "<<y<<" "<<z<<endl;
+                        }
+                    }
+                }
+            }   
+        }
+    }
 }
 
 int main(){
@@ -170,14 +145,14 @@ int main(){
         int a;
         cin >> a >> X[i] >> Y[i];
     }
-    //solve2D(k, n, m, X,Y, s);
-    newSolve2D(k, n, m, X,Y);
-    //k =4;
-    //m = 3;
-    //n=3;
-    //int p = 3;
-    //int X[] = {1,2,2,1};
-    //int Y[] = {3,2,1,2};
-    //int Z[] = {3,2,2,1};
-    //solve3D(k, n, m, p, X, Y, Z, s);
+
+    Solve2D(k, n, m, X,Y);
+    k =4;
+    m = 3;
+    n=3;
+    int o = 3;
+    int X2[] = {1,2,2,1};
+    int Y2[] = {3,2,1,2};
+    int Z2[] = {3,2,2,1};
+    solve3D(k, n, m, o, X2, Y2, Z2);
 }
