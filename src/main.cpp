cca57f8a3b1b33286032e0c45147c275607b16c6
Romain Fontaine <romain-fontaine@outlook.com>
2016-05-19 15:31:38 +0200

    flottaison en "D

--- src/main.cpp
+++ src/main.cpp
@@ -146,7 +146,7 @@ void Solve2DRetournement(int k, int n, int m, int *X, int *Y){
     }
 }
 
-void solve3D(int k, int n, int m, int o, int *X, int *Y, int *Z){
+void solve3D(int k, int n, int m, int o, int *X, int *Y, int *Z, bool flottant=false){
     Solver s;
     int mu[k][n][m][o][2];
     for(int i=0; i<k; i++){
@@ -162,20 +162,28 @@ void solve3D(int k, int n, int m, int o, int *X, int *Y, int *Z){
 
     for(int i=0; i<k; i++){
         vec<Lit> solution_exists;
-        for(int x0=0; x0<=n-X[i]; x0++){
-            for(int y0=0; y0<=m-Y[i]; y0++){
-                for(int z0=0; z0<=o-Z[i]; z0++){
+        for(int x0=0; x0<n; x0++){
+            for(int y0=0; y0<m; y0++){
+                for(int z0=0; z0<o; z0++){
                     solution_exists.push(Lit(mu[i][x0][y0][z0][0]));
-                    for(int x1=0; x1<n; x1++){
-                        for(int y1=0; y1<m; y1++){
-                            for(int z1=0; z1<o; z1++){
-                                if(x0<=x1 && x1<(x0+X[i])
-                                        && y0 <= y1 && y1<(y0+Y[i])
-                                        && z0 <= z1 && z1<(z0+Z[i])){
-                                    s.addBinary(~Lit(mu[i][x0][y0][z0][0]), Lit(mu[i][x1][y1][z1][1]));
+                    if(x0<=m-X[i] && y0<=m-Y[i] && z0<=o-Z[i]){
+                        for(int x1=0; x1<n; x1++){
+                            for(int y1=0; y1<m; y1++){
+                                for(int z1=0; z1<o; z1++){
+                                    if(x0<=x1 && x1<(x0+X[i])
+                                            && y0 <= y1 && y1<(y0+Y[i])
+                                            && z0 <= z1 && z1<(z0+Z[i])){
+                                        s.addBinary(~Lit(mu[i][x0][y0][z0][0]), Lit(mu[i][x1][y1][z1][1]));
+                                    }
+                                    if(x0!=x1 && y0!=y1 && z0!=z1){
+                                        s.addBinary(~Lit(mu[i][x0][y0][z0][0]), ~Lit(mu[i][x1][y1][z1][0]));
+                                    }
                                 }
                             }
+
                         }
+                    }else{
+                        s.addUnit(~Lit(mu[i][x0][y0][z0][0]));
                     }
                 }
             }
@@ -197,6 +205,41 @@ void solve3D(int k, int n, int m, int o, int *X, int *Y, int *Z){
         }
     }
 
+    if(flottant){
+        for(int x=0; x<n; x++){
+            for(int y=0; y<m; y++){
+                for(int z=1; z<o; z++){
+                    for(int i=0; i<k; i++){
+                        vec<Lit> nonFloating;
+                        nonFloating.push(~Lit(mu[i][x][y][z][1]));
+                        for(int j=0; j<k; j++){
+                            nonFloating.push(Lit(mu[j][x][y][z-1][1]));
+                        }
+                        s.addClause(nonFloating);
+                    }
+                }
+            }
+        }
+        for(int x=0; x<n; x++){
+            for(int y=0; y<m; y++){
+                for(int z=0; z<o; z++){
+                    for(int i=0; i<k; i++){
+                        vec<Lit> reverseImpl;
+                        reverseImpl.push(~Lit(mu[i][x][y][z][1]));
+                        for(int x1=x; x1>=0 && x1>x-X[i]; x1--){
+                            for(int y1=y; y1>=0 && y1>y-Y[i]; y1--){
+                                for(int z1=z; z1>=0 && z1>z-Z[i]; z1--){
+                                    reverseImpl.push(Lit(mu[i][x1][y1][z1][0]));
+                                }
+                            }
+                        }
+                        s.addClause(reverseImpl);
+                    }
+                }
+            }
+        }
+    }
+
     if(!s.solve() || !s.okay()){
         cout << 0 << endl;
         exit(42);
@@ -208,7 +251,7 @@ void solve3D(int k, int n, int m, int o, int *X, int *Y, int *Z){
                 for(int y=0; y<m; y++){
                     for(int z=0; z<o; z++){
                         if(s.model[mu[i][x][y][z][0]] == l_True){
-                            cout << ""<< i + 1 <<" "<<x<<" "<<y<<" "<<z<<endl;
+                            cout << i + 1 <<" "<< x<<" "<<y<<" "<<z<<" "<<endl;
                         }
                     }
                 }
@@ -228,7 +271,10 @@ int main(int argc, char *argv[]){
             } else if (argv[1][1] == 'r'){
                 mode = 'r';
             }
+            else if (argv[1][1] == 'f'){
+                mode = 'f';
         }
+    }
     }
 
     if(mode == '2'){
@@ -260,7 +306,10 @@ int main(int argc, char *argv[]){
             cin >> a >> X[i] >> Y[i];
         }
         Solve2DRetournement(k, n, m, X,Y);
-    } else if (mode == '3') {
+    } else if (mode == '3' || mode == 'f') {
+        bool floating = false;
+        if(mode == 'f')
+            floating = true;
         cout << "| Solving in 3D mode" << endl;
         int k = 4;
         int m = 3;
@@ -269,6 +318,6 @@ int main(int argc, char *argv[]){
         int X2[] = {1,2,2,1};
         int Y2[] = {3,2,1,2};
         int Z2[] = {3,2,2,1};
-        solve3D(k, n, m, o, X2, Y2, Z2);
+        solve3D(k, n, m, o, X2, Y2, Z2, floating);
     }
 }
