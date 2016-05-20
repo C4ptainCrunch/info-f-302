99f3f1196336c49a2205d31ae159b780f5697025
Romain Fontaine <romain-fontaine@outlook.com>
2016-05-20 10:55:12 +0200

    combinaisons

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,62 @@
 #include <iostream>
 #include <cmath>
+#include <algorithm>
+#include <vector>
+#include <list>
+#include <tuple>
+#include <numeric>
 
 #include "Solver.hpp"
+
+
 
 inline int max(int x, int y){
     return (x > y) ? x : y;
 }
 
+
+template <typename Iterator>
+   inline bool next_combination(const Iterator first, Iterator k, const Iterator last)
+   {
+      if ((first == last) || (first == k) || (last == k))
+         return false;
+      Iterator itr1 = first;
+      Iterator itr2 = last;
+      ++itr1;
+      if (last == itr1)
+         return false;
+      itr1 = last;
+      --itr1;
+      itr1 = k;
+      --itr2;
+      while (first != itr1)
+      {
+         if (*--itr1 < *itr2)
+         {
+            Iterator j = k;
+            while (!(*itr1 < *j)) ++j;
+            std::iter_swap(itr1,j);
+            ++itr1;
+            ++j;
+            itr2 = k;
+            std::rotate(itr1,j,last);
+            while (last != j)
+            {
+               ++j;
+               ++itr2;
+            }
+            std::rotate(k,itr2,last);
+            return true;
+         }
+      }
+      std::rotate(first,k,last);
+      return false;
+   }
+
+
 using namespace std;
 
-void Solve2D(int k, int n, int m, int *X, int *Y){
+void Solve2D(int k, int n, int m, int *X, int *Y, int p=0){
     Solver s;
     int mu[k][n][m][2];
     for(int i=0; i<k; i++){
@@ -29,10 +76,13 @@ void Solve2D(int k, int n, int m, int *X, int *Y){
                 for(int x1=0; x1<n; x1++){
                     for(int y1=0; y1<m; y1++){
                         if(x0<=x1 && x1<(x0+X[i]) && y0 <= y1 && y1<(y0+Y[i])){
+                            // gama(i, x0, y0) -> beta(i, x1, y1) un départ implique un block
                             s.addBinary(~Lit(mu[i][x0][y0][0]), Lit(mu[i][x1][y1][1]));
                         }
                     }
                 }
+
+                // beta(i, x0, y0) -> V(gama(i, x1, y1)) chaque block doit avoir un départ
                 vec<Lit> reverseImpl;
                 reverseImpl.push(~Lit(mu[i][x0][y0][1]));
                 for(int x1=x0; x1>=0 && x1>x0-X[i]; x1--){
@@ -46,16 +96,57 @@ void Solve2D(int k, int n, int m, int *X, int *Y){
         s.addClause(solution_exists);
     }
 
+    for(int x0=0; x0<n; x0++){
+        for(int y0=0; y0<m; y0++){
+            for(int x1=0; x1<n; x1++){
+                for(int y1=0; y1<m; y1++){
+                    if(x0 == x1 && y0==y1)
+                        continue;
+                    for(int i=0; i<k; i++){
+                        s.addBinary(~Lit(mu[i][x0][y0][0]), ~Lit(mu[i][x1][y1][0]));
+                    }
+                }
+            }
+        }
+    }
+
     for(int i=0; i<k; i++){
         for(int j=0; j<k; j++){
             if(i == j)
                 continue;
             for(int x=0; x<n; x++){
                 for(int y=0; y<m; y++){
+                    // beta(i, x, y) -> ~beta(j, x, y) on  ne peut avoir q'un seul block en x,y
                     s.addBinary(~Lit(mu[i][x][y][1]), ~Lit(mu[j][x][y][1]));
                 }
             }
         }
+    }
+
+    if(p>0 && p<=(m+n)*2){
+        vector<tuple<int, int, int>> borders((m+n)*2);
+        for(int x=0; x<n; x++){
+            borders[x] = make_tuple(x, x,0);
+            borders[n+x] = make_tuple(n+x, x,m-1);
+        }
+        for(int y=0; y<m; y++){
+            cerr << "y: "<< y << endl;
+            borders[2*n+y] = make_tuple(2*n+y, 0,y);
+            borders[2*n+m+y] = make_tuple(2*n+m+y, n-1,y);
+        }
+        int c = borders.size()- p + 1;
+        cerr << c << " " << borders.size()<<endl;
+        do{
+            vec<Lit> b;
+            for(int w=0; w<c; w++){
+                for(int i=0; i<k; i++){
+                    b.push(Lit(mu[i][get<1>(borders[w])][get<2>(borders[w])][1]));
+                }
+                cerr << get<1>(borders[w])<< " " <<get<2>(borders[w])<<"\t";
+            }
+            cerr << endl;
+            s.addClause(b);
+        } while(next_combination(borders.begin(), borders.begin() + c, borders.end()));
     }
 
     if(!s.solve() || !s.okay()){
@@ -72,6 +163,23 @@ void Solve2D(int k, int n, int m, int *X, int *Y){
                     }
                 }
             }
+        }
+        cerr << endl << endl;
+        for(int x=n-1; x>=0; x--){
+            for(int y=0; y<m; y++){
+                bool f = false;
+                for(int i =0; i<k; i++){
+                    if(s.model[mu[i][x][y][1]] == l_True){
+                        cerr <<  i + 1 <<" ";
+                        f=true;
+                        break;
+                    }
+                } if(!f) {
+                    cerr <<  0 <<" ";
+                }
+
+            }
+            cerr << endl;
         }
     }
 }
@@ -299,7 +407,7 @@ int main(int argc, char *argv[]){
             cin >> a >> X[i] >> Y[i];
         }
 
-        Solve2D(k, n, m, X,Y);
+        Solve2D(k, n, m, X,Y, 14);
     } else if(mode == 'r'){
         cout << "| Solving in swap mode" << endl;
         Solver s;
